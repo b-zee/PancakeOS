fc81d8143ce2c87b3e809aebb0add4efdea272a0
bennoz <bzeeman@live.nl>
2014-03-19 20:25:31 +0100

    First attempt at a custom OS
    With help from tutorials from the net.

---
+++ Raspberry Pi OS/src/main.s
@@ -0,0 +1,34 @@
+.section .init
+.globl _start
+_start:
+	ldr r0,=0x20200000
+
+	mov r1,#1
+	lsl r1,#18
+	str r1,[r0,#4]
+
+	loop$:
+		mov r1,#0
+		lsl r1,#16
+		str r1,[r0,#40]
+
+		// High value to substract from
+		mov r2,#0x3F0000
+		wait1$:
+			sub r2,#1
+			cmp r2,#0
+			bne wait1$
+
+		mov r1,#0
+		lsl r1,#16
+		str r1,[r0,#40]
+
+		// High value to substract from
+		mov r2,#0x3F0000
+		wait2$:
+			sub r2,#1
+			cmp r2,#0
+			bne wait2$
+
+
+		b loop$
