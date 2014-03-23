4ffeccdb250cf9172c234e4636e45cf772466329
bennoz <bzeeman@live.nl>
2014-03-23 18:47:16 +0100

    Still figuring out my build environment

--- Raspberry Pi OS/src/main.s
+++ Raspberry Pi OS/src/main.s
@@ -1,34 +1,33 @@
 .section .init
 .globl _start
 _start:
-	ldr r0,=0x20200000
 
-	mov r1,#1
-	lsl r1,#18
-	str r1,[r0,#4]
+ldr r0,=0x20200000
 
-	loop$:
-		mov r1,#0
-		lsl r1,#16
-		str r1,[r0,#40]
+mov r1,#1
+lsl r1,#18
 
-		// High value to substract from
-		mov r2,#0x3F0000
-		wait1$:
-			sub r2,#1
-			cmp r2,#0
-			bne wait1$
+str r1,[r0,#4]
 
-		mov r1,#0
-		lsl r1,#16
-		str r1,[r0,#40]
+mov r1,#1
+lsl r1,#16
 
-		// High value to substract from
-		mov r2,#0x3F0000
-		wait2$:
-			sub r2,#1
-			cmp r2,#0
-			bne wait2$
+loop$: 
 
+	str r1,[r0,#0x1C]
 
-		b loop$
+	mov r2,#0x3F0000
+	wait1$:
+		sub r2,#1
+		cmp r2,#0
+		bne wait1$
+
+	str r1,[r0,#0x28]
+
+	mov r2,#0x3F0000
+	wait2$:
+		sub r2,#1
+		cmp r2,#0
+		bne wait2$
+
+	b loop$
