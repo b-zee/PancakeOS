440d69e70a0b3795a6791bdaff2414b0ff976984
bennoz <bzeeman@live.nl>
2014-03-29 23:38:16 +0100

    Couple of new files
    Still experimenting with an optimal build environment.

--- Raspberry Pi OS/src/rpi_startup.c
+++ Raspberry Pi OS/src/rpi_startup.c
@@ -1,15 +1,15 @@
-//extern int __bss_start__;
-//extern int __bss_end__;
+extern int __bss_start;
+extern int __bss_end;
 
 extern int main();
 
 void _rpi_startup()
 {
-//    int* bss = &__bss_start__;
-//    int* bss_end = &__bss_end__;
+    int* bss = &__bss_start;
+    int* bss_end = &__bss_end;
 
-//    while( bss < bss_end )
-//        *bss++ = 0;
+    while( bss < bss_end )
+        *bss++ = 0;
 
     main();
 
