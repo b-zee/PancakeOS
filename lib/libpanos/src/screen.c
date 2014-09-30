243be7c348dc013d54618beb8414bdf598c6cac1
bennoz <bzeeman@live.nl>
2014-09-30 13:01:36 +0200

    Using mailbox.s to fix temporarily

--- lib/libpanos/src/screen.c
+++ lib/libpanos/src/screen.c
@@ -1,17 +1,15 @@
 #include "screen.h"
-
-static void panos_mailbox_write_wait();
-static void panos_mailbox_read_wait();
-static void panos_mailbox_write(uint8_t channel, uint32_t data);
-static void panos_mailbox_read(uint8_t channel, uint32_t *data);
-
 #include "error.h"
 
-#define MAILBOX_ADDRESS		(unsigned int *)0x2000B880
+//void panos_mailbox_write_wait();
+//void panos_mailbox_read_wait();
+void panos_mailbox_write(uint32_t data, uint8_t channel);
+uint32_t panos_mailbox_read(uint8_t channel);
 
-#define MAIL_READ_ADDRESS	MAILBOX_ADDRESS + 0x00
-#define MAIL_STATUS_ADDRESS	MAILBOX_ADDRESS + 0x18
-#define MAIL_WRITE_ADDRESS	MAILBOX_ADDRESS + 0x20
+#define MAIL_ADDRESS		(unsigned int *)0x2000B880
+#define MAIL_READ_ADDRESS	MAIL_ADDRESS + 0x00
+#define MAIL_STATUS_ADDRESS	MAIL_ADDRESS + 0x18
+#define MAIL_WRITE_ADDRESS	MAIL_ADDRESS + 0x20
 
 struct FRAME_BUFFER {
     volatile uint32_t width;
@@ -26,10 +24,10 @@ struct FRAME_BUFFER {
     volatile uint32_t size;
 };
 
+static struct FRAME_BUFFER panos_fb __attribute__ ((aligned (16))) = {0};
+
 int panos_screen_initialize(uint16_t width, uint16_t height, uint8_t depth)
 {
-	static struct FRAME_BUFFER panos_fb __attribute__ ((aligned (16))) = {0};
-
 	panos_fb.width			= (uint32_t)width;
 	panos_fb.height			= (uint32_t)height;
 	panos_fb.virtual_width	= (uint32_t)width;
@@ -41,12 +39,11 @@ int panos_screen_initialize(uint16_t width, uint16_t height, uint8_t depth)
     panos_fb.pointer		= 0;
     panos_fb.size			= 0;
 
-	panos_mailbox_write(0x1, (uint32_t)&panos_fb);
+	panos_mailbox_write((uint32_t)&panos_fb | 0x40000000, 0x1);
 
-	uint32_t data;
-	panos_mailbox_read(0x1, &data);
+	uint32_t data = panos_mailbox_read(0x1);
 
-	if (data != 0) {
+	if (data != 0 || panos_fb.pointer == 0) {
 		return PANOS_ERROR;
 	}
 
@@ -54,7 +51,7 @@ int panos_screen_initialize(uint16_t width, uint16_t height, uint8_t depth)
 }
 
 // Static functions
-void panos_mailbox_write_wait()
+/*void panos_mailbox_write_wait()
 {
 	// Loop while MAIL_FULL
 	while ((*(MAIL_STATUS_ADDRESS) & 0x80000000) != 0);
@@ -78,7 +75,7 @@ void panos_mailbox_read(uint8_t channel, uint32_t *data)
 		panos_mailbox_read_wait();
 
 		*data = *(MAIL_READ_ADDRESS);
-	} while ((*data & 0x0000000F) != (uint32_t)channel);
+	} while (((*data) & 0x0000000F) != (uint32_t)channel);
 
 	*data &= 0xFFFFFFF0;
-}
+}*/
